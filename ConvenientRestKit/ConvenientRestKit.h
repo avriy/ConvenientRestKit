2ca7833582293d7b6dbe567d227a39efb6c8c6df
Artemiy Sobolev <avriy@me.com>
2017-06-03 23:17:34 +0300

    Add xcodeproj for supporting Carthage

---
+++ ConvenientRestKit/ConvenientRestKit.h
@@ -0,0 +1,19 @@
+//
+//  ConvenientRestKit.h
+//  ConvenientRestKit
+//
+//  Created by Artemiy Sobolev on 02/06/2017.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ConvenientRestKit.
+FOUNDATION_EXPORT double ConvenientRestKitVersionNumber;
+
+//! Project version string for ConvenientRestKit.
+FOUNDATION_EXPORT const unsigned char ConvenientRestKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConvenientRestKit/PublicHeader.h>
+
+
